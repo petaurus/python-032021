8b42e2a3e574a2d51d3741a679466c76b66607cc
petaurus <Markova.com@gmail.com>
2021-10-14 22:49:54 +0200

    Ukol 1.lekce, zakomentovat nepotrebne casti

--- Ukoly/1_titanic.py
+++ Ukoly/1_titanic.py
@@ -2,8 +2,8 @@ import pandas
 import requests
 import numpy
 
-r = requests.get("https://raw.githubusercontent.com/pesikj/progr2-python/master/data/titanic.csv")
-open("titanic.csv", 'wb').write(r.content)
+# r = requests.get("https://raw.githubusercontent.com/pesikj/progr2-python/master/data/titanic.csv")
+# open("titanic.csv", 'wb').write(r.content)
 
 titanic = pandas.read_csv("titanic.csv")
 titanic_pivot = pandas.pivot_table(titanic, values="Survived", index="Pclass", columns="Sex", aggfunc=numpy.sum, margins=True)
