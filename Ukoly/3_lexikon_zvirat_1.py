80cd71862e9dcab7dfef1dbaae9163d9661962c4
petaurus <Markova.com@gmail.com>
2021-10-31 14:38:07 +0100

    Ukol c.2

---
+++ Ukoly/3_lexikon_zvirat_1.py
@@ -0,0 +1,24 @@
+import requests
+import pandas
+
+r = requests.get("https://raw.githubusercontent.com/lutydlitatova/czechitas-datasets/main/datasets/lexikon-zvirat.csv")
+open("lexikon-zvirat.csv", "wb").write(r.content)
+
+lexikon = pandas.read_csv("lexikon-zvirat.csv", sep=";")
+
+lexikon = lexikon.dropna(how="all", axis="columns")
+lexikon = lexikon.dropna(how="all", axis="rows")
+lexikon = lexikon.set_index("id")
+
+def check_url(radek):
+    url = radek["image_src"]
+    if isinstance(url, str) and url.startswith("https://zoopraha.cz/images/") and url.lower().endswith("jpg"):
+        return True
+
+neplatne_url = pandas.DataFrame(columns=["nazev_zvirete"])
+
+for idx, zvire in lexikon.iterrows():
+    nazev_zvirete = zvire["title"]
+    if not check_url(zvire):
+        neplatne_url = neplatne_url.append({"nazev_zvirete": nazev_zvirete}, ignore_index=True)
+print(neplatne_url)
---
+++ Ukoly/3_lexikon_zvirat_2.py
@@ -0,0 +1,24 @@
+import requests
+import pandas
+
+r = requests.get("https://raw.githubusercontent.com/lutydlitatova/czechitas-datasets/main/datasets/lexikon-zvirat.csv")
+open("lexikon-zvirat.csv", "wb").write(r.content)
+
+lexikon = pandas.read_csv("lexikon-zvirat.csv", sep=";")
+
+lexikon = lexikon.dropna(how="all", axis="columns")
+lexikon = lexikon.dropna(how="all", axis="rows")
+lexikon = lexikon.set_index("id")
+
+def popisek(radek):
+    zvire = radek["title"]
+    strava = radek["food"]
+    typ_stravy = radek["food_note"]
+    znaky_zvirete = radek["description"]
+    return f"{zvire} preferuje následující typ stravy: {strava}. Konkrétně ocení když mu do misky přistanou {typ_stravy}. \nJak toto zvíře poznáme: {znaky_zvirete}."
+
+lexikon["popisek"] = lexikon.apply(popisek, axis=1)
+print(lexikon["popisek"])
+
+print(lexikon["popisek"][320])
+print(lexikon["popisek"][300])
